c6b3226c805247e03854bf015872858d13d53b95
osmanuzen <osmanuzen60@gmail.com>
2021-08-26 12:38:55 -0400

    test2

---
+++ src/_025_IfExampleWith0.java
@@ -0,0 +1,17 @@
+import sun.jvmstat.perfdata.monitor.PerfStringVariableMonitor;
+
+public class _025_IfExampleWith0 {
+    public static void main(String[] args) {
+
+        java.util.Scanner kb = new java.util.Scanner(System.in);
+        int n;
+        System.out.println("N değeri giriniz: ");
+        n = kb.nextInt();
+        if ( n > 0)
+            System.out.println("0 değildir. ");
+
+
+
+
+    }
+}
