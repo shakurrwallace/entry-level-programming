a4a80d9fe4514e35489302e94820de4369c108f3
osmanuzen <osmanuzen60@gmail.com>
2021-09-13 16:26:20 -0400

    test2

--- src/_066_DiziExample19.java
+++ src/_066_DiziExample19.java
@@ -5,27 +5,27 @@
 public class _066_DiziExample19 {
     public static void main(String[] args) {
         int a[] = {4, 8, 3, 1, 18, 9, 21, 20, 5, 17};
-        int i, n, size,cnt, cnt2, sum, sum2;
+        int i, n, size,cntOdd, cntEven, sumOdd, sumEven;
         size = 10;
-        cnt = 0;
-        sum = 0;
-        sum2 = 0;
-        cnt2 = 0;
+        cntOdd = 0;
+        sumOdd = 0;
+        sumEven = 0;
+        cntEven = 0;
         java.util.Scanner kb = new java.util.Scanner(System.in);
         System.out.println("Bir sayı giriniz: ");
         n = kb.nextInt();
         for (i = 0; i < size; i = i + 1 )
             if ( a[i] < n)
                 if (a[i] % 2 == 0) {
-                    sum2 = sum2 + a[i];
-                    cnt2 = cnt2 + 1;
+                    sumEven = sumEven + a[i];
+                    cntEven = cntEven + 1;
                 } else {
-                    sum = sum + a[i];
-                    cnt = cnt + 1;
+                    sumOdd = sumOdd + a[i];
+                    cntOdd = cntOdd + 1;
                 }
-        System.out.println("Çift sayılar toplamı: " + sum2);
-        System.out.println("Çift sayılar adetleri: " + cnt2);
-        System.out.println("Tek sayılar toplamı: " + sum);
-        System.out.println("Tek sayılar adetleri: " + cnt);
+        System.out.println("Çift sayılar toplamı: " + sumEven);
+        System.out.println("Çift sayılar adetleri: " + cntEven);
+        System.out.println("Tek sayılar toplamı: " + sumOdd);
+        System.out.println("Tek sayılar adetleri: " + cntOdd);
     }
 }
