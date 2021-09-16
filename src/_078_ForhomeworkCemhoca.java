f7970afb4e8fd0cee9cb35d7fde3ce9f60407809
osmanuzen <osmanuzen60@gmail.com>
2021-09-16 15:58:40 -0400

    test2

--- src/_078_ForhomeworkCemhoca.java
+++ src/_078_ForhomeworkCemhoca.java
@@ -10,12 +10,16 @@ public class _078_ForhomeworkCemhoca {
         n = n  % size;
         n2= 2009;
         System.out.println("Birler basamağı değeri: " + n);
+
         n2 = n2 / 1000;
         System.out.println("Binler basamağı değeri: " + n2);
+
         sum = n + n2;
         System.out.println("2 ile 9 u toplarsan: " + sum );
+
         sum2 = n2 * size + n;
         System.out.println("Yan yana yazarsan: " + sum2);
+
         bahceli = sum + sum2;
         System.out.println(bahceli + " yaparrrr");
 
